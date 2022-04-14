083cc3ceaf15d9b49ee775b98bcf41b0d7c3fd85
Denislav <deni_9000_1990@abv.bg>
2022-04-14 16:14:30 +0300

    new phrase

--- ROBOWEB/first/views.py
+++ ROBOWEB/first/views.py
@@ -94,6 +94,10 @@ class RegistrationView(views.CreateView):
 @login_required()
 def login_page(request):
     """this page is important, show login, control panel, video, all"""
+    new_fraze=Phrase(phrase="ddd")
+    new_fraze.save()
+    new_frazee = Phrase(phrase="ddd")
+    new_frazee.save()
     status_video=StateVideo.STATE_VIDEO
     #current_user =Profile.objects.get(pk=get_user_model().id)
     current_user =Profile.objects.get(pk=request.user.id)
