31063d22cbc0cade0dea6b9d0add3b4dfb317512
Denislav <deni_9000_1990@abv.bg>
2022-04-14 13:55:12 +0300

    fixed whitenoise

--- ROBOWEB/settings.py
+++ ROBOWEB/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = DJANGO_APPS+PROJECT_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -139,7 +140,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_ROOT=os.path.join(BASE_DIR/'media/')
 MEDIA_URL='/media/'
